ba7c344f71758cc66189cfd2aa2cab616bb97031
Im-Beast <franik.mateusz@gmail.com>
2025-05-13 19:49:07 +0200

    fix(android): set proper target version

--- android/app/src/androidTest/java/com/ianthetechie/foobar/ExampleInstrumentedTest.kt
+++ android/app/src/androidTest/java/com/ianthetechie/foobar/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("app.unimusic.sync", appContext.packageName)
+    assertEquals("app.unimusic.sync.demo", appContext.packageName)
   }
 }
