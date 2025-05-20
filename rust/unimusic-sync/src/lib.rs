8ef1efb396951bc35dad02cf8c3aae874341be54
Im-Beast <franik.mateusz@gmail.com>
2025-05-20 21:42:28 +0200

    feat(rust): connection test

--- rust/unimusic-sync/src/lib.rs
+++ rust/unimusic-sync/src/lib.rs
@@ -363,3 +363,243 @@ impl IrohManager {
         Ok(replica.id().into())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::{IrohFactory, IrohManager};
+    use log::{error, info, warn};
+    use std::{
+        error::Error,
+        path::{Path, PathBuf},
+        sync::Arc,
+        time::{Duration, SystemTime},
+    };
+    use tokio::task::{JoinHandle, JoinSet};
+
+    type Result<T> = std::result::Result<T, Box<dyn Error + Sync + Send>>;
+
+    static MODIFIED_DOGS: &[u8] = "German Shephard, Husky, Pomeranian".as_bytes();
+    static TEST_FILES: [(&str, &[u8]); 2] = [
+        (
+            "dog_breeds.txt",
+            "American Eskimo Dog, Husky, Cocker Spaniel, Pomeranian".as_bytes(),
+        ),
+        (
+            "bing chilling.txt",
+            r#"現在我有冰淇淋
+        我很喜歡冰淇淋
+        但是
+        《速度與激情9》
+        比冰淇淋
+        《速度與激-》
+        《速度與激情9》
+        我最喜歡
+        所以現在是
+        音樂時間
+        準備
+
+        一
+        二
+        三
+
+        兩個禮拜以後
+        《速度與激情9》
+        兩個禮拜以後
+        《速度與激情9》
+        兩個禮拜以後
+        《速度與激情9》
+
+        不要忘記
+        不要錯過
+        去電影院
+        看《速度與激情9》
+        因為非常好電影
+        動作非常好
+        差不多一樣「冰激淋」
+        再見"#
+                .as_bytes(),
+        ),
+    ];
+
+    struct TempDir(PathBuf);
+    impl TempDir {
+        pub fn new<P: AsRef<Path>>(path: P) -> Self {
+            Self(std::env::temp_dir().join(path))
+        }
+
+        pub fn subpath<P: AsRef<Path>>(&self, path: P) -> PathBuf {
+            self.0.join(path)
+        }
+    }
+    impl Drop for TempDir {
+        fn drop(&mut self) {
+            let _ = std::fs::remove_dir_all(&self.0);
+        }
+    }
+
+    async fn mock_client(dir: PathBuf) -> Result<IrohManager> {
+        let iroh_manager = IrohFactory::new()
+            .iroh_manager(dir.to_string_lossy().into_owned())
+            .await?;
+        Ok(iroh_manager)
+    }
+
+    #[tokio::test]
+    async fn test_connection() -> Result<()> {
+        env_logger::init();
+
+        let sys_time = std::time::SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_secs();
+        let temp_dir = TempDir::new(sys_time.to_string());
+
+        let provider = mock_client(temp_dir.subpath("provider")).await?;
+
+        info!("[provider] create namespace");
+        let namespace = provider.create_namespace().await?;
+
+        info!("[provider] write files");
+        let mut file_hashes = Vec::new();
+        for (path, contents) in TEST_FILES {
+            assert!(provider.read_file(namespace, path).await.is_err());
+
+            let file_hash = provider
+                .write_file(namespace, path.to_string(), contents.to_vec())
+                .await?;
+            assert_eq!(provider.read_file_hash(file_hash).await?, contents);
+            assert_eq!(provider.read_file(namespace, path).await?, contents);
+
+            file_hashes.push(file_hash);
+        }
+
+        info!("[provider] share ticke");
+        let ticket = provider.share(namespace).await?;
+
+        let mut set = JoinSet::new();
+        let ticket = Arc::new(ticket);
+        let file_hashes = Arc::new(file_hashes);
+
+        info!("[receivers] test 5 concurrent connections");
+        for i in 0..5 {
+            let ticket = ticket.clone();
+            let file_hashes = file_hashes.clone();
+            let receiver_path = temp_dir.subpath(format!("receiver_{i}"));
+            let task: JoinHandle<Result<()>> = tokio::spawn(async move {
+                let receiver = mock_client(receiver_path).await?;
+
+                info!("[receiver {i}]: make sure files are empty before import:");
+                info!("[receiver {i}]: via hash");
+                for file_hash in file_hashes.iter() {
+                    assert!(receiver.read_file_hash(*file_hash).await.is_err());
+                }
+                info!("[receiver {i}]: via path");
+                for (file_path, _) in TEST_FILES {
+                    assert!(receiver.read_file(namespace, file_path).await.is_err());
+                }
+
+                info!("[receiver {i}]: import ticket");
+                let imported_namespace = receiver.import((*ticket).clone()).await?;
+                assert_eq!(namespace, imported_namespace);
+
+                info!("[receiver {i}]: make sure files are got properly imported");
+                for (i, (path, contents)) in TEST_FILES.iter().enumerate() {
+                    info!("[receiver {i}]: ... properly imported {path}");
+                    assert_eq!(&receiver.read_file_hash(file_hashes[i]).await?, contents);
+                    assert_eq!(&receiver.read_file(namespace, path).await?, contents);
+                }
+
+                info!("[receiver {i}]: shutdown");
+                receiver.shutdown().await?;
+                Ok(())
+            });
+            set.spawn(task);
+        }
+
+        for result in set.join_all().await {
+            result.unwrap()?;
+        }
+
+        info!("[provider]: modify dogs.txt");
+        provider
+            .write_file(
+                namespace,
+                TEST_FILES[0].0.to_string(),
+                MODIFIED_DOGS.to_vec(),
+            )
+            .await?;
+        assert_eq!(
+            provider.read_file(namespace, TEST_FILES[0].0).await?,
+            MODIFIED_DOGS
+        );
+
+        let mut set = JoinSet::new();
+        for i in 0..5 {
+            let file_hashes = file_hashes.clone();
+            let provider_id = provider.router.endpoint().node_id().into();
+            let receiver_path = temp_dir.subpath(format!("receiver_{i}"));
+            let task: JoinHandle<Result<IrohManager>> = tokio::spawn(async move {
+                info!("[receiver {i}]: recreate");
+                let receiver = mock_client(receiver_path).await?;
+
+                info!("[receiver {i}]: make sure files are still there");
+                for (i, (path, contents)) in TEST_FILES.iter().enumerate() {
+                    assert_eq!(&receiver.read_file_hash(file_hashes[i]).await?, contents);
+                    assert_eq!(&receiver.read_file(namespace, path).await?, contents);
+                }
+
+                info!("[receiver {i}]: reconnect");
+                receiver.reconnect().await;
+                let mut retries = 0u32;
+                while !receiver.get_known_nodes().await.contains(&provider_id) {
+                    if retries > 15 {
+                        error!("[receiver {i}]: didn't find provider after 15 tries, giving up");
+                        break;
+                    }
+
+                    warn!("[receiver {i}]: didn't find provider, retrying in 2s...");
+                    tokio::time::sleep(Duration::from_secs(2)).await;
+                    receiver.reconnect().await;
+
+                    retries += 1;
+                }
+
+                info!("[receiver {i}]: sync");
+                retries = 0;
+                while let Err(_) = receiver.sync(namespace).await {
+                    if retries > 15 {
+                        error!("[receiver {i}]: failed sync 15 times, giving up");
+                        break;
+                    }
+                    warn!("[receiver {i}]: sync failed, retrying in 2s...");
+                    tokio::time::sleep(Duration::from_secs(2)).await;
+                }
+
+                info!("[receiver {i}]: synced, wait 5 seconds for it to propagate...");
+                tokio::time::sleep(Duration::from_secs(5)).await;
+
+                info!("[receiver {i}]: make sure file got properly synced");
+                assert_eq!(
+                    &receiver.read_file(namespace, TEST_FILES[0].0).await?,
+                    MODIFIED_DOGS
+                );
+
+                Ok(receiver)
+            });
+            set.spawn(task);
+        }
+
+        let mut receivers = Vec::new();
+        for result in set.join_all().await {
+            receivers.push(result.unwrap()?);
+        }
+
+        info!("[receivers]: shutdown");
+        for receiver in receivers {
+            receiver.shutdown().await?;
+        }
+        info!("[provider]: shutdown");
+        provider.shutdown().await?;
+
+        Ok(())
+    }
+}
