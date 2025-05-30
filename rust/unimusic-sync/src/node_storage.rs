7e24f6cbbbf37f5d53ce174d0679fe3a05f71d55
Im-Beast <franik.mateusz@gmail.com>
2025-05-30 14:27:59 +0200

    chore(rust): log information about node_storage operations

--- rust/unimusic-sync/src/node_storage.rs
+++ rust/unimusic-sync/src/node_storage.rs
@@ -1,4 +1,5 @@
 use iroh::{NodeId, node_info::NodeData};
+use log::info;
 use serde::{Deserialize, Serialize};
 use std::{borrow::Cow, collections::HashMap, path::Path};
 use tokio::fs;
@@ -15,8 +16,9 @@ impl NodeStorage {
     /// Load NodeStorage from given path, or create a new instance if it doesn't exist
     pub async fn load<P: AsRef<Path>>(path: P) -> Result<Self> {
         if let Ok(file) = fs::read(path).await {
-            let deserialized =
+            let deserialized: Self =
                 serde_json::from_slice(&file).map_err(|e| SharedError::Serde(e.to_string()))?;
+            info!("[node_storage]: loaded {} nodes", deserialized.nodes.len());
             Ok(deserialized)
         } else {
             Ok(Self::default())
@@ -27,13 +29,16 @@ impl NodeStorage {
     pub async fn save<P: AsRef<Path>>(&self, path: P) -> Result<()> {
         let serialized =
             serde_json::to_vec_pretty(self).map_err(|e| SharedError::Serde(e.to_string()))?;
+        info!("[node_storage]: saving {} nodes", self.nodes.len());
         fs::write(path, serialized).await?;
+        info!("[node_storage]: saved {} nodes", self.nodes.len());
         Ok(())
     }
 
     /// Update or insert a new data
     /// Updated info merges with the previous one to hold more information about the node
     pub fn upsert_node(&mut self, id: NodeId, new_data: Cow<NodeData>) {
+        info!("[node_storage]: upserted new node: {}", id.fmt_short());
         self.nodes
             .entry(id.into())
             .and_modify(|data| {
